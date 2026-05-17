dcd4083b1dea91a7587f8d810fc630d02eb3afd6
Yevhen Zarkhin <zarkhineugene@gmail.com>
2026-05-17 17:43:32 +0300

    fix inconsistent declaring

--- shared/src/stores/member.ts
+++ shared/src/stores/member.ts
@@ -7,7 +7,8 @@ export type Member = MemberFormValues & {
   id: string;
   createdAt: number;
 };
-function generateUUID() {
+
+const generateUUID = () => {
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
     var r = (Math.random() * 16) | 0;
     var v = c == "x" ? r : (r & 0x3) | 0x8;
